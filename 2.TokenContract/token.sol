ecf42fa594e21c07e7c4cb490b25627569b52798
root <dragonetail@gmail.com>
2018-04-12 16:58:12 +0000

    update

--- 2.TokenContract/token.sol
+++ 2.TokenContract/token.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.16;
+pragma solidity ^0.4.21;
 
 contract Token {
     address issuer;
@@ -7,25 +7,27 @@ contract Token {
     event Issue(address account, uint amount);
     event Transfer(address from, address to, uint amount);
 
-    function Token() {
+    function Token() public {
         issuer = msg.sender;
     }
 
-    function issue(address account, uint amount) {
+    function issue(address account, uint amount) public {
         require(msg.sender == issuer);
         balances[account] += amount;
+
+        emit Issue(account, amount);
     }
 
-    function transfer(address to, uint amount) {
+    function transfer(address to, uint amount) public {
         require(balances[msg.sender] >= amount);
 
         balances[msg.sender] -= amount;
         balances[to] += amount;
 
-        Transfer(msg.sender, to, amount);
+        emit Transfer(msg.sender, to, amount);
     }
 
-    function getBalance(address account) constant returns (uint) {
+    function getBalance(address account) public constant returns (uint) {
         return balances[account];
     }
 }
